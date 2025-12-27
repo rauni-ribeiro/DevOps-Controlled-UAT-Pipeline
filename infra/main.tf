36d4cf62282d16a4702c24afd15a41dab534ed63
Rauni <raunirr98@gmail.com>
2025-12-27 15:59:02 -0300

    updating: main.tf - resolving blob_name var referencing

--- infra/main.tf
+++ infra/main.tf
@@ -19,5 +19,6 @@ module "backend_storage" {
   location             = var.location
   storage_account_name = var.storage_account_name
   container_name       = var.container_name
+  blob_name            = var.blob_name
   tags                 = var.tags
 }
